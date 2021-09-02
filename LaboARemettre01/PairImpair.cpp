f171aca4dc40099a8af4ca995e7b2d14f3ae2657
wnault <2148080@cstj.qc.ca>
2021-09-02 13:13:38 -0400

    Ajoutez des fichiers projet.

---
+++ LaboARemettre01/PairImpair.cpp
@@ -0,0 +1,32 @@
+// But : Écrire un programme qui lit un entier et qui détermine et affiche
+// s'il sagit d'un entier ou pair d'un entier impair.
+// Auteur : William Nault
+// Date : 2021-09-01
+
+#include<iostream>
+
+void main()
+{
+	int nb1;
+
+	setlocale(LC_ALL, "");
+
+	// Demmander a l'utilisateur de rentrer un nombre entier
+
+	std::cout << "Entrer un nombre entier :";
+	std::cin >> nb1;
+
+	// Déterminer si le nombre est pair ou impair en divisant par 2 et que le reste égal 0
+
+	if (nb1 % 2 == 0)
+	{
+		std::cout << nb1 << " est un nombre pair" << std::endl;
+	}
+	else 
+	{
+		std::cout << nb1 << " est un nombre impair.";
+	}
+
+
+	//509212 (509212 est un nombre pair), 000000 (0 est un nombre pair),00000000001 (1 est un nombre impair)
+}
